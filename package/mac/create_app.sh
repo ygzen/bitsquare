62057212435a54d8d3fe257d9dd4acb691c533d3
Manfred Karrer <mk@nucleo.io>
2016-07-07 03:08:52 +0200

    Update scripts

--- package/mac/create_app.sh
+++ package/mac/create_app.sh
@@ -7,26 +7,34 @@ version="0.4.9"
 cd ../../
 
 projectDir=$(pwd)
-
 deployDir="$projectDir/gui/deploy"
 customJDK="$deployDir/jdk1.8.92_minimized.jdk"
 customJavaHome="$customJDK/Contents/Home"
 
-mkdir -p gui/deploy
-
+echo "---- Build jar files ----"
 mvn clean package -DskipTests -Dmaven.javadoc.skip=true
 
+echo "---- Copy jar files deploy dir ----"
+mkdir -p gui/deploy
 cp gui/target/shaded.jar "gui/deploy/Bitsquare-$version.jar"
 cp seednode/target/SeedNode.jar "gui/deploy/SeedNode-$version.jar"
 
+echo "---- Copy Bitsquare jar file to VM shared folders ----"
 cp gui/target/shaded.jar "/Users/mk/vm_shared_ubuntu/Bitsquare-$version.jar"
 cp gui/target/shaded.jar "/Users/mk/vm_shared_windows/Bitsquare-$version.jar"
 cp gui/target/shaded.jar "/Users/mk/vm_shared_ubuntu14_32bit/Bitsquare-$version.jar"
 cp gui/target/shaded.jar "/Users/mk/vm_shared_windows_32bit/Bitsquare-$version.jar"
 
-echo "JAVA_HOME: $JAVA_HOME"
-echo "CustomJavaHome $customJavaHome"
+echo "---- Copy hotfix jar file to VM shared folders ----"
+cp $customJavaHome/jre/lib/ext/jfxrt.jar "/Users/mk/vm_shared_ubuntu/jfxrt.jar"
+cp $customJavaHome/jre/lib/ext/jfxrt.jar "/Users/mk/vm_shared_windows/jfxrt.jar"
+cp $customJavaHome/jre/lib/ext/jfxrt.jar "/Users/mk/vm_shared_ubuntu14_32bit/jfxrt.jar"
+cp $customJavaHome/jre/lib/ext/jfxrt.jar "/Users/mk/vm_shared_windows_32bit/jfxrt.jar"
 
+echo "JAVA_HOME: $JAVA_HOME"
+echo "CustomJavaHome: $customJavaHome"
+
+echo "---- Build dmg file ----"
 $JAVA_HOME/bin/javapackager \
     -deploy \
     -BappVersion=$version \
@@ -44,6 +52,7 @@ $JAVA_HOME/bin/javapackager \
     -outfile Bitsquare \
     -BjvmProperties=-Djava.net.preferIPv4Stack=true
 
+echo "---- Copy dmg file to deploy dir, clean up ----"
 mv "gui/deploy/bundles/Bitsquare-$version.dmg" "gui/deploy/Bitsquare-$version.dmg"
 rm "gui/deploy/Bitsquare.html"
 rm "gui/deploy/Bitsquare.jnlp"
--- package/windows/create_64bit_app.bat
+++ package/windows/create_64bit_app.bat
@@ -1,4 +1,4 @@
-﻿﻿:: edit iss file -> AppVersion
+﻿:: edit iss file -> AppVersion
 
 :: Copy gui/deploy.Bitsquare.jar file from mac build to windows
 :: edit -> -BappVersion and -srcfiles
@@ -11,7 +11,6 @@ SET jdk=C:\Program Files\Java\jdk1.8.0_92
 SET outdir=\\VBOXSVR\vm_shared_windows
 
 call "%jdk%\bin\javapackager.exe" -deploy ^
--BjvmOptions=-Xbootclasspath/a:^"jdkfix-0.4.9.jar^";^"..\runtime\lib\ext\jfxrt.jar^" ^
 -BappVersion="%version%" ^
 -native exe ^
 -name Bitsquare ^
@@ -19,12 +18,7 @@ call "%jdk%\bin\javapackager.exe" -deploy ^
 -vendor Bitsquare ^
 -outdir %outdir% ^
 -appclass io.bitsquare.app.BitsquareAppMain ^
--srcfiles "%outdir%\Bitsquare-%version%.jar;%outdir%\jdkfix-%version%.jar" ^
+-srcfiles "%outdir%\Bitsquare-%version%.jar" ^
 -outfile Bitsquare ^
 -Bruntime="%jdk%\jre" ^
 -BjvmProperties=-Djava.net.preferIPv4Stack=true
-
-:: -BjvmOptions=-verbose:class
-:: those works
-:: java -Xbootclasspath/a:^"jdkfix-0.4.9.jar^";^"..\runtime\lib\ext\jfxrt.jar^" -jar Bitsquare-0.4.9.jar
-:: java -Xbootclasspath/a:"jdkfix-0.4.9.jar";"..\runtime\lib\ext\jfxrt.jar" -jar Bitsquare-0.4.9.jar
